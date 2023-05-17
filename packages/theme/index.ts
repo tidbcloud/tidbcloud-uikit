619b93d1ea08f457a9c4f8b9326e4af983cdee4e
zoubingwu <zoubingwu@gmail.com>
2023-05-17 17:39:22 +0800

    fix theme exports

--- packages/theme/index.ts
+++ packages/theme/index.ts
@@ -1,16 +1,10 @@
-import { useMantineColorScheme } from '@mantine/core'
-
-import { Colors as LightColors } from './colors'
-import { Colors as DarkColors } from './colors.dark'
-
 export * from './colors'
 export { themeColors as darkThemeColors, Colors as DarkColors } from './colors.dark'
+
 export * from './fns'
+
 export { FONT_FAMILY } from './font'
+
 export { theme as Theme } from './theme'
 
-export function useColors() {
-  const { colorScheme } = useMantineColorScheme()
-  const Colors = colorScheme === 'dark' ? DarkColors : LightColors
-  return Colors
-}
+export * from './Chart'
