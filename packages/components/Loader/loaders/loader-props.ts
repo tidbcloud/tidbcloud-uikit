c0ceeac5b13571ee07336def6dd1cab5198d8e98
awxxxxxx <dwaterbear@163.com>
2023-06-07 16:52:31 +0800

    refactor: remove packages/components

--- packages/components/Loader/loaders/loader-props.ts
+++
@@ -1,7 +0,0 @@
-import type { MantineColor } from '@mantine/styles'
-import React from 'react'
-
-export interface LoaderProps extends React.ComponentPropsWithoutRef<'svg'> {
-  size: number
-  color: MantineColor
-}
