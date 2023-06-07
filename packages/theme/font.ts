4dd22af9c9d194cfbabc4bee46d5c7fea98247a3
awxxxxxx <dwaterbear@163.com>
2023-06-07 16:58:15 +0800

    refactor: remove packages/theme

--- packages/theme/font.ts
+++
@@ -1,2 +0,0 @@
-export const FONT_FAMILY =
-  'Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Arial, Noto Sans SC, Microsoft Yahei, sans-serif, Menlo, Consolas, monospace, Apple Color Emoji, Segoe UI Emoji, emoji'
