a1361671d68bf8364c3593abbe8644b82672037a
awxxxxxx <dwaterbear@163.com>
2023-06-07 16:57:04 +0800

    refactor: remove packages/icons

--- packages/icons/react/XClose.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-const XClose = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
-    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 6 6 18M6 6l12 12" />
-  </svg>
-)
-export default XClose
