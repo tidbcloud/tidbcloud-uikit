45a60f271e916556be6368938a5f8a5e4f170851
awxxxxxx <dwaterbear@163.com>
2023-06-07 17:41:37 +0800

    feat: add publish github actions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,7 @@
-import type { StorybookConfig } from '@storybook/react-vite'
+import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-dark-mode'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-dark-mode', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/react-vite',
     options: {}
@@ -14,5 +9,5 @@ const config: StorybookConfig = {
   docs: {
     autodocs: 'tag'
   }
-}
-export default config
+};
+export default config;
