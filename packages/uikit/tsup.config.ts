c453d7e2adc14cbc2cbd7bf9c46e6c082237b6ee
zoubingwu <zoubingwu@gmail.com>
2023-07-19 15:50:40 +0800

    fix: entry point

--- packages/uikit/tsup.config.ts
+++ packages/uikit/tsup.config.ts
@@ -1,23 +1,11 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: {
-    carousel: 'src/carousel/index.ts',
-    index: 'src/components/index.ts',
-    dates: 'src/dates/index.ts',
-    hooks: 'src/hooks/index.ts',
-    icons: 'src/icons/index.ts',
-    modals: 'src/modals/index.ts',
-    notifications: 'src/notifications/index.ts',
-    prism: 'src/prism/index.ts',
-    theme: 'src/theme/index.ts',
-    utils: 'src/utils/index.ts',
-    dropzone: 'src/dropzone/index.ts'
-  },
-  format: ['esm', 'cjs'],
+  entry: ['src/**/*.{ts,tsx}'],
+  format: ['esm'],
   sourcemap: true,
   clean: true,
   dts: true,
-  bundle: true,
+  bundle: false,
   external: ['react', 'react-dom']
 })
