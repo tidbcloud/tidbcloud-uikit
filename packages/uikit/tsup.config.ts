c721fa12546150289ef67c8f780538d064c0f35e
zoubingwu <zoubingwu@gmail.com>
2023-07-19 17:04:30 +0800

    fix: tweak tsup config

--- packages/uikit/tsup.config.ts
+++ packages/uikit/tsup.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/**/*.{ts,tsx}'],
-  format: ['esm'],
+  entry: ['src/**/*.{ts,tsx,js,jsx}'],
+  format: ['esm', 'cjs'],
   sourcemap: true,
   clean: true,
   dts: true,
   bundle: false,
-  external: ['react', 'react-dom']
+  external: ['react', 'react-dom'],
+  onSuccess: 'pnpm run copy:raw-icon'
 })
