a4f9343888ee2476523281e7d8553f5ee191dda4
zoubingwu <zoubingwu@gmail.com>
2023-07-27 13:55:55 +0800

    fix: sync uikit changes from dbaas

--- packages/uikit/src/icons/react/CurrencyDollarCircle.jsx
+++ packages/uikit/src/icons/react/CurrencyDollarCircle.jsx
@@ -1,11 +1,19 @@
 import * as React from 'react'
 const CurrencyDollarCircle = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    strokeWidth={1.5}
+    viewBox="0 0 24 24"
+    {...props}
+  >
     <path
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={2}
+      strokeWidth="inherit"
       d="M8.5 14.667A2.333 2.333 0 0 0 10.833 17H13a2.5 2.5 0 0 0 0-5h-2a2.5 2.5 0 0 1 0-5h2.167A2.333 2.333 0 0 1 15.5 9.333M12 5.5V7m0 10v1.5M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2s10 4.477 10 10Z"
     />
   </svg>
