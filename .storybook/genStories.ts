c6a680a0a47c1796e90026955d82232aa55ba059
awxxxxxx <dwaterbear@163.com>
2023-08-08 17:46:57 +0800

    feat(storybook): auto gen stories

--- .storybook/genStories.ts
+++ .storybook/genStories.ts
@@ -1,30 +1,39 @@
 import { glob } from 'glob'
 import fs from 'node:fs/promises'
 import path from 'path'
-import ts, { couldStartTrivia } from 'typescript'
+import ts from 'typescript'
 
-const getProperties = (checker: ts.TypeChecker, symbol: ts.Symbol) => {
-  const defaultExportType = checker.getTypeOfSymbolAtLocation(symbol, symbol.declarations![0])
-  const properties = defaultExportType.getProperties()
-  return properties
+const getPropsTypes = (properties: ts.Symbol[], checker: ts.TypeChecker, symbol: ts.Symbol) => {
+  properties.forEach((prop) => {
+    if (prop.escapedName === 'propTypes' && symbol.declarations?.[0]) {
+      const propType = checker.getTypeOfSymbolAtLocation(prop, symbol.declarations[0])
+      // console.log(
+      checker
+        .getPropertiesOfType(propType)
+        .filter((p) => {})
+        .map((t) => t.escapedName)
+      // )
+    }
+  })
 }
 
 const isJSXElment = (properties: ts.Symbol[]) => {
   const names = ['defaultProps', 'propTypes', 'displayName']
-  return properties.some((property) => names.includes(property.escapedName!))
+  return properties.some((prop) => {
+    return names.includes(prop.escapedName!)
+  })
 }
 
-const genereateStory = async (comp: string) => {
+const genereateStory = async (comp: string, imported: string) => {
   const template = `
 import type { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
-import { ${comp} } from '@tidbcloud/uikit'
+import { ${comp} } from '${imported}'
 
 const meta: Meta<typeof ${comp}> = {
   title: 'Biz/Form',
   component: ${comp},
-  parameters: {
-  }
+  parameters: {}
 }
 
 export default meta
@@ -32,14 +41,9 @@ export default meta
 type Story = StoryObj<typeof ${comp}>
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-export const ControlledForm: Story = {
-  args: {
-    children: (
-      <div>
-        <${comp} />
-      </div>
-    )
-  }
+export const Primary${comp}: Story = {
+  render: () => (<${comp}></${comp}>),
+  args: {}
 }
 `
   const filePath = `${process.cwd()}/stories/uikit/${comp}.stories.tsx`
@@ -51,8 +55,24 @@ export const ControlledForm: Story = {
   })
 }
 
+const resolveImportPath = (file: string) => {
+  const defaultPath = '@tidbcloud/uikit'
+  const paths: [RegExp, string][] = [
+    [/theme/, 'theme'],
+    [/icons/, 'icons']
+  ]
+  for (let i = 0; i < paths.length; i++) {
+    const [regexp, name] = paths[i]
+    if (regexp.test(file)) {
+      return `${defaultPath}/${name}`
+    }
+  }
+
+  return defaultPath
+}
+
 const getAllPaths = async () => {
-  const paths = glob.sync(`${process.cwd()}/packages/uikit/src/components/index.ts`, {
+  const paths = glob.sync(`${process.cwd()}/packages/uikit/src/**/index.{ts,tsx}`, {
     ignore: ['**/utils/**', '**/theme/**', '**/hooks/**']
   })
 
@@ -75,11 +95,10 @@ const getAllPaths = async () => {
     symbols.forEach((symbol) => {
       const type = checker.getTypeOfSymbolAtLocation(symbol, sourceFile)
       const properties = type.getProperties()
-      // @ts-ignore
       if (!isJSXElment(properties)) {
         return
       }
-      genereateStory(symbol.escapedName!)
+      genereateStory(symbol.escapedName!, resolveImportPath(file))
     })
   })
 }
