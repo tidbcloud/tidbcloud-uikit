edc0b71f23150dad00d426f1ab9bde5c34187fc0
baurine <2008.hbl@gmail.com>
2023-08-16 15:02:07 +0800

    docs(storybook): add stories for some primitive elements

--- stories/uikit/List.stories.tsx
+++ stories/uikit/List.stories.tsx
@@ -20,8 +20,36 @@ const meta: Meta<typeof List> = {
 
 export default meta
 
+function PrimaryDemo() {
+  return (
+    <List listStyleType="disc">
+      <List.Item>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+      </List.Item>
+      <List.Item>First order item</List.Item>
+      <List.Item>
+        First order item with list
+        <List withPadding listStyleType="disc">
+          <List.Item>Nested item</List.Item>
+          <List.Item>Nested item</List.Item>
+          <List.Item>
+            Nested item with list
+            <List withPadding listStyleType="disc">
+              <List.Item>Event more nested</List.Item>
+              <List.Item>Event more nested</List.Item>
+            </List>
+          </List.Item>
+          <List.Item>Nested item</List.Item>
+        </List>
+      </List.Item>
+      <List.Item>First order item</List.Item>
+    </List>
+  )
+}
+
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Primary: Story = {
-  render: () => <List></List>,
+  render: () => <PrimaryDemo />,
   args: {}
 }
--- stories/uikit/Title.stories.tsx
+++ stories/uikit/Title.stories.tsx
@@ -20,8 +20,21 @@ const meta: Meta<typeof Title> = {
 
 export default meta
 
+function PrimaryDemo() {
+  return (
+    <>
+      <Title order={1}>This is h1 title</Title>
+      <Title order={2}>This is h2 title</Title>
+      <Title order={3}>This is h3 title</Title>
+      <Title order={4}>This is h4 title</Title>
+      <Title order={5}>This is h5 title</Title>
+      <Title order={6}>This is h6 title</Title>
+    </>
+  )
+}
+
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Primary: Story = {
-  render: () => <Title></Title>,
+  render: () => <PrimaryDemo />,
   args: {}
 }
