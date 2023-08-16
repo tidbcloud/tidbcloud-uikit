85a120b9f5285838576ca73db94aee8d1a25fdb1
Suhaha <jklopsdfw@gmail.com>
2023-08-16 12:23:42 +0800

    story: add some primitive component

--- stories/uikit/FocusTrap.stories.tsx
+++ stories/uikit/FocusTrap.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj, StoryFn } from '@storybook/react'
-import { FocusTrap } from '@tidbcloud/uikit'
+import { Box, FocusTrap, Button, TextInput } from '@tidbcloud/uikit'
+import { useDisclosure } from '@tidbcloud/uikit/hooks'
 
 type Story = StoryObj<typeof FocusTrap>
 
@@ -20,8 +21,26 @@ const meta: Meta<typeof FocusTrap> = {
 
 export default meta
 
+function PrimaryDemo() {
+  const [active, { toggle }] = useDisclosure(false)
+
+  return (
+    <Box maw={400} mx="auto">
+      <Button onClick={toggle}>{active ? 'Deactivate' : 'Activate'} focus trap</Button>
+
+      <FocusTrap active={active}>
+        <div>
+          <TextInput mt="sm" label="First input" placeholder="First input" />
+          <TextInput mt="sm" label="Second input" placeholder="Second input" />
+          <TextInput mt="sm" label="Third input" placeholder="Third input" />
+        </div>
+      </FocusTrap>
+    </Box>
+  )
+}
+
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Primary: Story = {
-  render: () => <FocusTrap></FocusTrap>,
+  render: () => <PrimaryDemo />,
   args: {}
 }
