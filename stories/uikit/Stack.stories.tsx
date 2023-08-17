2e94cbcf51db6d3ed6278103ad5c5c99772b54c1
Bosn <bosn@outlook.com>
2023-08-17 10:18:48 +0800

    feat: add args for the button

--- stories/uikit/Stack.stories.tsx
+++ stories/uikit/Stack.stories.tsx
@@ -22,15 +22,30 @@ export default meta
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Primary: Story = {
-  render: () => (
+  render: ({ ...rest }) => (
     <Stack
       h={300}
       sx={(theme) => ({ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0] })}
+      {...rest}
     >
       <Button variant="outline">1</Button>
       <Button variant="outline">2</Button>
       <Button variant="outline">3</Button>
     </Stack>
   ),
-  args: {}
+  args: {},
+  argTypes: {
+    align: {
+      options: ['stretch', 'flex-start', 'flex-end', 'center'],
+      control: { type: 'select' }
+    },
+    spacing: {
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+      control: { type: 'select' }
+    },
+    justify: {
+      options: ['flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'space-evenly'],
+      control: { type: 'select' }
+    }
+  }
 }
