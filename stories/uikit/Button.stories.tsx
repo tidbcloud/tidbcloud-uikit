5d5aeb8fc4eb39aea101aba0ce32215a6f174c2d
sanshuiyijing <jingdeveloper@126.com>
2023-08-22 19:43:53 +0800

    feat: uikit button & actionIcon

--- stories/uikit/Button.stories.tsx
+++ stories/uikit/Button.stories.tsx
@@ -1,6 +1,8 @@
 import type { Meta, StoryObj, StoryFn } from '@storybook/react'
 import { Button, Stack } from '@tidbcloud/uikit'
 
+import { COLOR_LIST, VARIANT_LIST, SIZE_LIST } from '../constants'
+
 type Story = StoryObj<typeof Button>
 
 const decorator = (Story: StoryFn) => {
@@ -22,14 +24,134 @@ export default meta
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Primary: Story = {
-  render: () => (
-    <Stack align="flex-start">
-      <Button>Click Me!</Button>
-      <Button variant="default">variant is default</Button>
-      <Button variant="light">variant is light</Button>
-      <Button variant="outline">variant is outline</Button>
-      <Button variant="subtle">variant is subtle</Button>
+  render: ({ ...rest }) => (
+    <Stack
+      align="flex-start"
+      sx={{
+        backgroundColor: (rest as any).variant === 'white' ? '#ddd' : 'transparent',
+        padding: 16
+      }}
+    >
+      <Button color="blue" variant="filled" {...rest}>
+        Settings
+      </Button>
     </Stack>
   ),
-  args: {}
+  parameters: {
+    controls: {
+      expanded: true
+    }
+  },
+  args: {},
+  argTypes: {
+    color: {
+      options: COLOR_LIST,
+      control: { type: 'select' }
+    },
+    variant: {
+      options: VARIANT_LIST.concat('white'),
+      control: { type: 'select' }
+    },
+    gradient: {
+      control: 'object',
+      description: `<div> Controls gradient settings in gradient variant only. Type is MantineGradient.</div>
+<div>example: {from: 'color-from', to: 'color-to', deg: 120}</div>`,
+      table: {
+        type: {
+          summary: 'MantineGradient'
+        }
+      },
+      if: {
+        arg: 'variant',
+        eq: 'gradient'
+      }
+    },
+    radius: {
+      options: SIZE_LIST,
+      control: {
+        type: 'select'
+      }
+    },
+    size: {
+      options: SIZE_LIST,
+      control: { type: 'select' }
+    },
+    disabled: {
+      control: 'boolean'
+    },
+    loading: {
+      control: 'boolean'
+    },
+    loaderPosition: {
+      options: ['left', 'right', 'center'],
+      control: { type: 'select' },
+      if: {
+        arg: 'loading',
+        truthy: true
+      }
+    },
+    loaderProps: {
+      control: 'object',
+      if: {
+        arg: 'loading',
+        truthy: true
+      },
+      description:
+        'Props spread to Loader component, type is LoaderProps, example: {size: "sm", color: "red", variant: "filled"'
+    },
+    compact: {
+      control: 'boolean'
+    },
+    uppercase: {
+      control: 'boolean'
+    },
+    fullWidth: {
+      control: 'boolean'
+    },
+    leftIcon: {
+      control: { type: 'text' },
+      description: 'Adds icon before button label',
+      table: {
+        type: {
+          summary: 'ReactNode'
+        }
+      }
+    },
+    rightIcon: {
+      control: { type: 'text' },
+      description: 'Adds icon after button label',
+      table: {
+        type: {
+          summary: 'ReactNode'
+        }
+      }
+    },
+    type: {
+      options: ['button', 'submit', 'reset'],
+      control: { type: 'select' },
+      defaultValue: 'button'
+    }
+  }
+}
+
+export const Secondary: Story = {
+  render: ({ ...rest }) => (
+    <Stack align="flex-start">
+      <Button.Group orientation="horizontal" buttonBorderWidth={1} {...rest}>
+        <Button variant="default">First</Button>
+        <Button variant="default">Second</Button>
+        <Button variant="default">Third</Button>
+      </Button.Group>
+    </Stack>
+  ),
+  args: {},
+  argTypes: {
+    orientation: {
+      options: ['horizontal', 'vertical'],
+      control: { type: 'select' }
+    },
+    buttonBorderWidth: {
+      control: { type: 'number' }
+    }
+  }
 }
