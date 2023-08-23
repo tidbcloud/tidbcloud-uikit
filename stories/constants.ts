17f79373392c6c0d5e9469805b32e6ddd8b02fb3
sanshuiyijing <jingdeveloper@126.com>
2023-08-23 15:23:12 +0800

    feat: uikit button & actionIcon (#36)

---
+++ stories/constants.ts
@@ -0,0 +1,18 @@
+export const COLOR_LIST = [
+  'dark',
+  'gray',
+  'pink',
+  'red',
+  'grape',
+  'violet',
+  'indigo',
+  'blue',
+  'cyan',
+  'teal',
+  'green',
+  'lime',
+  'yellow',
+  'orange'
+]
+export const VARIANT_LIST = ['outline', 'transparent', 'light', 'default', 'filled', 'gradient', 'subtle']
+export const SIZE_LIST = ['xs', 'sm', 'md', 'lg', 'xl']
--- stories/uikit/CloseButton.stories.tsx
+++ stories/uikit/CloseButton.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj, StoryFn } from '@storybook/react'
-import { CloseButton } from '@tidbcloud/uikit'
+import { CloseButton, Stack } from '@tidbcloud/uikit'
+
+import { COLOR_LIST, VARIANT_LIST, SIZE_LIST } from '../constants'
 
 type Story = StoryObj<typeof CloseButton>
 
@@ -22,6 +24,85 @@ export default meta
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const Primary: Story = {
-  render: () => <CloseButton></CloseButton>,
-  args: {}
+  render: ({ ...rest }) => (
+    <Stack
+      align="flex-start"
+      sx={{
+        backgroundColor: (rest as any).variant === 'white' ? '#ddd' : 'transparent',
+        padding: 16
+      }}
+    >
+      <CloseButton {...rest} />
+    </Stack>
+  ),
+  parameters: {
+    controls: {
+      expanded: true
+    }
+  },
+  args: {},
+  argTypes: {
+    color: {
+      options: COLOR_LIST,
+      control: { type: 'select' }
+    },
+    variant: {
+      options: VARIANT_LIST.concat('white'),
+      control: { type: 'select' }
+    },
+    gradient: {
+      control: 'object',
+      description: `<div> Controls gradient settings in gradient variant only. Type is MantineGradient.</div>
+<div>example: {from: 'color-from', to: 'color-to', deg: 120}</div>`,
+      table: {
+        type: {
+          summary: 'MantineGradient'
+        }
+      },
+      if: {
+        arg: 'variant',
+        eq: 'gradient'
+      }
+    },
+    radius: {
+      options: SIZE_LIST,
+      control: {
+        type: 'select'
+      }
+    },
+    iconSize: {
+      control: {
+        type: 'number' || 'string'
+      },
+      description: 'Width and height of X icon',
+      table: {
+        type: {
+          summary: 'number | string'
+        }
+      }
+    },
+    size: {
+      options: SIZE_LIST,
+      control: { type: 'select' }
+    },
+    disabled: {
+      control: 'boolean'
+    },
+    loading: {
+      control: 'boolean'
+    },
+    loaderProps: {
+      control: 'object',
+      if: {
+        arg: 'loading',
+        truthy: true
+      },
+      description: 'Props spread to Loader component, example: {size: "sm", color: "red", variant: "filled"}',
+      table: {
+        type: {
+          summary: 'LoaderProps'
+        }
+      }
+    }
+  }
 }
