195563dc4c4db921c377a94aad60dcf903ceec60
zoubingwu <zoubingwu@gmail.com>
2023-08-25 15:08:45 +0800

    feat: migrate form components

---
+++ packages/uikit/src/biz/Form/RadioGroup.tsx
@@ -0,0 +1,67 @@
+import { get, omit } from 'lodash-es'
+import React, { Fragment } from 'react'
+import { Controller, RegisterOptions, useFormContext } from 'react-hook-form'
+
+import { Radio, RadioGroupProps, RadioProps, Box, HoverCard, HoverCardProps, Tooltip } from '../../primitive'
+
+export interface FormRadioGroupProps extends Omit<RadioGroupProps, 'children'> {
+  name: string
+  rules?: RegisterOptions
+  data: Array<RadioProps & { tooltip?: React.ReactNode; tooltipProps?: HoverCardProps & { useTooltip?: boolean } }>
+}
+
+export const FormRadioGroup: React.FC<FormRadioGroupProps> = ({ name, rules, onChange, data, ...rest }) => {
+  const { control, errors } = useFormContext()
+  const error = get(errors, name)
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={(field) => {
+        const { onChange: handleChange, ...restField } = field
+        return (
+          <Radio.Group
+            {...rest}
+            onChange={(value) => {
+              handleChange(value)
+              onChange?.(value)
+            }}
+            {...restField}
+            error={error?.message}
+          >
+            {data.map((i) => {
+              const radioProps = omit(i, 'tooltipProps', 'tooltip')
+              return (
+                <Fragment key={i.value as any}>
+                  {i.tooltip ? (
+                    i.tooltipProps?.useTooltip ? (
+                      <Tooltip label={i.tooltip} withArrow>
+                        <Box>
+                          <Radio {...radioProps} />
+                        </Box>
+                      </Tooltip>
+                    ) : (
+                      <HoverCard withArrow shadow="md" withinPortal {...i.tooltipProps}>
+                        <HoverCard.Target>
+                          <Box>
+                            <Radio {...radioProps} />
+                          </Box>
+                        </HoverCard.Target>
+
+                        <HoverCard.Dropdown>{i.tooltip}</HoverCard.Dropdown>
+                      </HoverCard>
+                    )
+                  ) : (
+                    <Radio {...radioProps} />
+                  )}
+                </Fragment>
+              )
+            })}
+          </Radio.Group>
+        )
+      }}
+    />
+  )
+}
---
+++ packages/uikit/src/biz/Form/TextInput.tsx
@@ -0,0 +1,36 @@
+import { get } from 'lodash-es'
+import { Controller, RegisterOptions, useFormContext } from 'react-hook-form'
+
+import { TextInput, TextInputProps } from '../../primitive'
+
+export interface FormTextInputProps extends TextInputProps {
+  name: string
+  rules?: RegisterOptions
+}
+
+export const FormTextInput: React.FC<FormTextInputProps> = ({ name, rules, onChange, ...rest }) => {
+  const { control, errors } = useFormContext()
+  const error = get(errors, name)
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={(field) => {
+        const { onChange: handleChange, ...restField } = field
+        return (
+          <TextInput
+            onChange={(e) => {
+              handleChange(e)
+              onChange?.(e)
+            }}
+            error={error?.message}
+            {...restField}
+            {...rest}
+          />
+        )
+      }}
+    />
+  )
+}
