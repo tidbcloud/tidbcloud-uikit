fca21fe07fc182de84d625f89ef5158f7a696346
Bosn <bosn@outlook.com>
2023-08-31 16:19:55 +0800

    feat: add search user story

---
+++ stories/uikit/SearchUser.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj, StoryFn } from '@storybook/react'
+import { CodeBlock, SearchArea } from '@tidbcloud/uikit/biz'
+
+type Story = StoryObj<typeof CodeBlock>
+
+const decorator = (Story: StoryFn) => {
+  return (
+    <div style={{ margin: '3em' }}>
+      <Story />
+    </div>
+  )
+}
+
+const meta: Meta<typeof CodeBlock> = {
+  title: 'Biz/SearchUser',
+  component: CodeBlock,
+  decorators: [decorator],
+  parameters: {}
+}
+
+export default meta
+
+const code = `
+import { CodeBlock } from '@tidbcloud/uikit';
+
+function Demo() {
+  return <CodeBlock>Put some real code here</CodeBlock>
+}
+`
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+export const Primary: Story = {
+  render: () => <SearchArea />,
+  args: {}
+}
