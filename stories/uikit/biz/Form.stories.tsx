f5db3cf6c70da8e586661f593421ee9f06aa6aa8
awxxxxxx <dwaterbear@163.com>
2023-09-01 17:30:00 +0800

    feat(story): add form story

---
+++ stories/uikit/biz/Form.stories.tsx
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj, StoryFn } from '@storybook/react'
+import { Form, FormTextInput, FormPasswordInput, FormMultiSelect, FormSelect } from '@tidbcloud/uikit/biz'
+import { Group, Center } from '@tidbcloud/uikit'
+import { Icon } from '@tidbcloud/uikit/icons'
+
+type Story = StoryObj<typeof Form>
+
+const decorator = (Story: StoryFn) => {
+  return (
+    <div style={{ margin: '3em' }}>
+      <Story />
+    </div>
+  )
+}
+
+const meta: Meta<typeof Form> = {
+  title: 'Biz/Form',
+  component: Form,
+  decorators: [decorator],
+  tags: ['autodocs'],
+  parameters: {}
+}
+
+export default meta
+
+// More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
+export const Primary: Story = {
+  render: ({ ...args }) => (
+    <Center w={500} m="0 auto">
+      <Form
+        onSubmit={() => {
+          throw new Error('Test error')
+        }}
+        w={'100%'}
+      >
+        <Group grow>
+          <FormTextInput name="first_name" required placeholder="Your first name" label="First name" />
+          <FormTextInput name="last_name" required placeholder="Your last name" label="Last name" />
+        </Group>
+        <FormTextInput
+          name="email"
+          mt="md"
+          required
+          placeholder="Your email"
+          label="Email"
+          icon={<Icon name="Atom01" size={16} stroke="1.5" />}
+        />
+        <FormPasswordInput
+          name="password"
+          mt="md"
+          required
+          placeholder="Password"
+          label="Password"
+          icon={<Icon name="Lock01" size={16} stroke="1.5" />}
+        />
+        <FormSelect
+          name="framework"
+          data={[
+            { label: 'React', value: 'react' },
+            { label: 'Vue', value: 'vue' }
+          ]}
+          label="Framework"
+        ></FormSelect>
+        <FormMultiSelect
+          name="framework"
+          data={[
+            { label: 'React', value: 'react' },
+            { label: 'Vue', value: 'vue' }
+          ]}
+          label="Multi Frameworks"
+        />
+      </Form>
+    </Center>
+  ),
+  args: {}
+}
