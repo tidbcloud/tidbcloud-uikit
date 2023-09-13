08c64d3346642a7c60ef1fcd17970cc5c16e2b19
awxxxxxx <dwaterbear@163.com>
2023-09-13 17:42:49 +0800

    fix(uikit): TextInput icon missing

---
+++ .changeset/tidy-rabbits-remain.md
@@ -0,0 +1,5 @@
+---
+'@tidbcloud/uikit': patch
+---
+
+fix text input icon missing
--- stories/uikit/primitive/TextInput.stories.tsx
+++ stories/uikit/primitive/TextInput.stories.tsx
@@ -26,7 +26,8 @@ export const Primary: Story = {
   render: ({ ...args }) => <TextInput {...args}></TextInput>,
   args: {
     leftLabel: 'prefix',
-    label: 'Input'
+    label: 'Input',
+    icon: 'Icon'
   }
 }
 
