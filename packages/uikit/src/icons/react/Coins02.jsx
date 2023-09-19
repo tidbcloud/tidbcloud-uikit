1b36e176cef87b05ef6fe92bfd824c582be48743
sanshuiyijing <jingdeveloper@126.com>
2023-09-19 16:49:40 +0800

    fix: change icon width

--- packages/uikit/src/icons/react/Coins02.jsx
+++ packages/uikit/src/icons/react/Coins02.jsx
@@ -6,7 +6,7 @@ const Coins02 = (props, ref) => (
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
-      strokeWidth={2}
+      strokeWidth={1.5}
       d="M15.938 15.938A7.001 7.001 0 0 0 15 2a7.001 7.001 0 0 0-6.938 6.062M7.5 13 9 12v5.5m-1.5 0h3M16 15a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
     />
   </svg>
