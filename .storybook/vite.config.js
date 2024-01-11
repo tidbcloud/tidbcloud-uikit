6cffa8dd18712cf43b14ec86b741feaf7737f6ae
zoubingwu <zoubingwu@gmail.com>
2024-01-11 18:33:36 +0800

    fix(storybook): only apply aliases in vite.config.js during dev (#124)

--- .storybook/vite.config.js
+++ .storybook/vite.config.js
@@ -2,16 +2,19 @@ import path from 'node:path'
 import process from 'node:process'
 import { defineConfig } from 'vite'
 
+const isDev = process.argv.includes('dev')
+const alias = [
+  { find: /^@tidbcloud\/uikit\/theme$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/theme') },
+  { find: /^@tidbcloud\/uikit\/hooks$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/hooks') },
+  { find: /^@tidbcloud\/uikit\/icons$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/icons') },
+  { find: /^@tidbcloud\/uikit\/biz$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/biz') },
+  { find: /^@tidbcloud\/uikit\/utils$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/utils') },
+  { find: /^@tidbcloud\/uikit$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/primitive') }
+]
+
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [
-      { find: /^@tidbcloud\/uikit\/theme$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/theme') },
-      { find: /^@tidbcloud\/uikit\/hooks$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/hooks') },
-      { find: /^@tidbcloud\/uikit\/icons$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/icons') },
-      { find: /^@tidbcloud\/uikit\/biz$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/biz') },
-      { find: /^@tidbcloud\/uikit\/utils$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/utils') },
-      { find: /^@tidbcloud\/uikit$/, replacement: path.resolve(process.cwd(), './packages/uikit/src/primitive') }
-    ]
+    alias: isDev ? alias : undefined
   }
 })
