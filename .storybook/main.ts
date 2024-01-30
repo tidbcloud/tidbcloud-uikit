339a5457d8a7e340aef44f6e9659e86b3e487489
zoubingwu <zoubingwu@gmail.com>
2024-01-31 00:09:21 +0800

    fix(storybook):  support darkmode for autodocs and upgrade storybook (#138)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    'storybook-dark-mode',
-    '@storybook/addon-mdx-gfm'
+    'storybook-dark-mode'
   ],
   framework: {
     name: '@storybook/react-vite',
