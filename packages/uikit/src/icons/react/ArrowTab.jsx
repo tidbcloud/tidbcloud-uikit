8c883b7d743045e127f62658088d2a486c4d15f4
Suhaha <jklopsdfw@gmail.com>
2024-02-06 13:37:33 +0800

    feat(uikit): add arrow tab icon (#145)

---
+++ packages/uikit/src/icons/react/ArrowTab.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { forwardRef } from 'react'
+const ArrowTab = (props, ref) => {
+  if (typeof props.size === 'number') {
+    const { size, ...rest } = props
+    props = {
+      ...rest,
+      height: size,
+      width: size
+    }
+  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      ref={ref}
+      {...props}
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.422 12h13.156m0 0-6-6m6 6-6 6m8-12v12"
+        strokeWidth="inherit"
+      />
+    </svg>
+  )
+}
+const ForwardRef = forwardRef(ArrowTab)
+export default ForwardRef
