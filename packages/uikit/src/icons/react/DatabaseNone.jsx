e707634b78119a7d2869233fda4843991454c8e9
sanshuiyijing <jingdeveloper@126.com>
2024-04-10 17:49:16 +0800

    fix: database-none icon dashed

--- packages/uikit/src/icons/react/DatabaseNone.jsx
+++ packages/uikit/src/icons/react/DatabaseNone.jsx
@@ -15,7 +15,7 @@ const DatabaseNone = (props, ref) => {
       width="1em"
       height="1em"
       fill="none"
-      viewBox="0 0 24 24"
+      viewBox="0 0 16 16"
       strokeWidth="1.5"
       ref={ref}
       {...props}
@@ -26,6 +26,7 @@ const DatabaseNone = (props, ref) => {
         stroke-linejoin="round"
         d="M14 3.33334C14 4.43791 11.3137 5.33334 8 5.33334C4.68629 5.33334 2 4.43791 2 3.33334M14 3.33334C14 2.22877 11.3137 1.33334 8 1.33334C4.68629 1.33334 2 2.22877 2 3.33334M14 3.33334V12.6667C14 13.7733 11.3333 14.6667 8 14.6667C4.66667 14.6667 2 13.7733 2 12.6667V3.33334M14 8.00001C14 9.10668 11.3333 10 8 10C4.66667 10 2 9.10668 2 8.00001"
         stroke-width="inherit"
+        stroke-dasharray="2 2"
       />
     </svg>
   )
