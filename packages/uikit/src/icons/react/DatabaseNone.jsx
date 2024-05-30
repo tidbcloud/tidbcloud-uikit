47ec16b2c3e698208e607bf1833be9bf788c07cd
zoubingwu <zoubingwu@gmail.com>
2024-05-30 09:32:01 +0800

    style: update stroke properties to camelCase in icon components

--- packages/uikit/src/icons/react/DatabaseNone.jsx
+++ packages/uikit/src/icons/react/DatabaseNone.jsx
@@ -22,11 +22,11 @@ const DatabaseNone = (props, ref) => {
     >
       <path
         stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M21 5C21 6.65685 16.9706 8 12 8C7.02944 8 3 6.65685 3 5M21 5C21 3.34315 16.9706 2 12 2C7.02944 2 3 3.34315 3 5M21 5L21 19C21 20.66 17 22 12 22C7 22 3 20.66 3 19L3 5M21 12C21 13.66 17 15 12 15C7 15 3 13.66 3 12"
-        stroke-width="inherit"
-        stroke-dasharray="2 2"
+        strokeWidth="inherit"
+        strokeDasharray="2 2"
       />
     </svg>
   )
