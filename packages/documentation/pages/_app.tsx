0fbbd6ae59961c8609a9aafe4090d3f7163e9f9f
zoubingwu <zoubingwu@gmail.com>
2024-06-11 19:22:33 +0800

    fix: notify next-theme about theme change

--- packages/documentation/pages/_app.tsx
+++ packages/documentation/pages/_app.tsx
@@ -1,9 +1,30 @@
 import { ThemeProvider } from '@components/ThemeProvider'
+import { useMount } from 'ahooks'
 import { AppProps } from 'next/app'
+import { useTheme } from 'next-themes'
 
 import '../globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { setTheme } = useTheme()
+
+  useMount(() => {
+    const targetNode = document.querySelector('html')
+    const config = { attributes: true }
+    const observer = new MutationObserver((mutationList) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
+          if (targetNode!.classList.contains('light')) {
+            setTheme('light')
+          } else if (targetNode!.classList.contains('dark')) {
+            setTheme('dark')
+          }
+        }
+      }
+    })
+    observer.observe(targetNode!, config)
+  })
+
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <Component {...pageProps} />
