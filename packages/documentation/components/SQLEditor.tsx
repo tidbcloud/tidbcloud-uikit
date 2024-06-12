8ce4996f578bd137b37b89b09ff6ce9b7ce0af63
Bosn <bosn@outlook.com>
2024-06-12 14:29:31 +0800

    feat: adjust space between the title & the desc

--- packages/documentation/components/SQLEditor.tsx
+++ packages/documentation/components/SQLEditor.tsx
@@ -14,7 +14,7 @@ export default function Component() {
           <div className="container space-y-10 xl:space-y-16 px-4 md:px-6">
             <div className="grid gap-4 md:grid-cols-2 md:gap-16">
               <div>
-                <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
+                <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem] mb-2">
                   Cloud Editor
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400 mb-8">
