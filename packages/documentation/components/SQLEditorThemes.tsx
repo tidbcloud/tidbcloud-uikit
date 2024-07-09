086cae9779a0f6d881a8d45d9a1c04ad52b6a370
sanshuiyijing <jingdeveloper@126.com>
2024-07-09 08:47:54 +0800

    feat: sql editor docs update

---
+++ packages/documentation/components/SQLEditorThemes.tsx
@@ -0,0 +1,31 @@
+import { EmbeddedStory } from '@components/EmbeddedStory'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@components/ui/tabs'
+
+const SQLEditorThemeChange = () => {
+  return (
+    <div>
+      <Tabs defaultValue="oneDark">
+        <TabsList>
+          <TabsTrigger value="oneDark">oneDark</TabsTrigger>
+          <TabsTrigger value="bbedit">bbedit</TabsTrigger>
+        </TabsList>
+        <TabsContent value="oneDark">
+          <EmbeddedStory
+            title="dark"
+            src={'https://tisqleditor-playground.netlify.app/?example=all&theme=oneDark'}
+            initHeight={400}
+          ></EmbeddedStory>
+        </TabsContent>
+        <TabsContent value="bbedit">
+          <EmbeddedStory
+            title="light"
+            src={'https://tisqleditor-playground.netlify.app/?example=all&theme=bbedit'}
+            initHeight={400}
+          ></EmbeddedStory>
+        </TabsContent>
+      </Tabs>
+    </div>
+  )
+}
+
+export default SQLEditorThemeChange
