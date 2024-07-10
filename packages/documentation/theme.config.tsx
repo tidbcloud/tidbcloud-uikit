413a6ef031267b3df96f5044ca48f77a7b036570
Bosn <bosn@outlook.com>
2024-07-10 17:52:11 +0800

    feat: add headers

--- packages/documentation/theme.config.tsx
+++ packages/documentation/theme.config.tsx
@@ -8,6 +8,21 @@ const logo = (
 
 const config: DocsThemeConfig = {
   logo,
+  head: (
+    <>
+      <link
+        rel="icon"
+        type="image/svg+xml"
+        href="https://boserverdisc.oss-cn-hangzhou.aliyuncs.com/public/tiui/favicon.png"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="TiUI - TiDB Cloud UI Library" />
+      <meta
+        property="og:description"
+        content="TiUI is the UI library for TiDB Cloud, which includes Cloud UI, SQL editor, icons. It enhances development efficiency and unifies UI style and interaction experience."
+      />
+    </>
+  ),
   project: {
     link: 'https://github.com/tidbcloud/tidbcloud-uikit'
   },
