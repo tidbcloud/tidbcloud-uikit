36c565f093c0e6d1052a2a8f1c32b8a618a1c120
baurine <2008.hbl@gmail.com>
2024-07-26 12:26:56 +0800

    feat(timerangepicker): step1

---
+++ packages/uikit/src/biz/TimeRangePicker/AbsoluteTimeRangePicker.tsx
@@ -0,0 +1,195 @@
+import {
+  Alert,
+  Button,
+  Flex,
+  Group,
+  Text,
+  Typography,
+  useMantineTheme,
+  DatePicker,
+  MonthSettings,
+  RangeCalendar,
+  TimeInput
+} from '@tidbcloud/uikit'
+import { IconChevronLeft, IconAlertCircle } from '@tidbcloud/uikit/icons'
+import dayjs from 'dayjs'
+import prettyMs from 'pretty-ms'
+import { CSSProperties, MouseEventHandler, useMemo, useState } from 'react'
+
+import { AbsoluteTimeRange, TimeRangeValue, timeFormatter } from './helpers.js'
+
+function formatDuration(seconds: number) {
+  let d = prettyMs(seconds * 1000, { verbose: true })
+  return d
+}
+
+interface AbsoluteTimeRangePickerProps {
+  value: TimeRangeValue
+  minDateTime?: Date
+  maxDateTime?: Date
+  maxDuration?: number // unit: seconds
+  onChange?: (v: AbsoluteTimeRange) => void
+  onCancel?: () => void
+  onReturnClick?: MouseEventHandler<HTMLElement>
+}
+
+const AbsoluteTimeRangePicker: React.FC<React.PropsWithChildren<AbsoluteTimeRangePickerProps>> = ({
+  value,
+  maxDateTime,
+  minDateTime,
+  maxDuration,
+  onChange,
+  onCancel,
+  onReturnClick
+}) => {
+  const theme = useMantineTheme()
+  const dayStyle: MonthSettings['dayStyle'] = (_date, modifiers) =>
+    (modifiers.weekend && !modifiers.disabled && !modifiers.selected && !modifiers.outside && !modifiers.selectedInRange
+      ? { color: theme.colors.gray[7] }
+      : null) as CSSProperties
+
+  const [start, setStart] = useState(() => new Date(value[0] * 1000))
+  const [end, setEnd] = useState(() => new Date(value[1] * 1000))
+
+  const startAfterEnd = useMemo(() => {
+    return start.valueOf() > end.valueOf()
+  }, [start, end])
+  const beyondMin = useMemo(() => {
+    return minDateTime && start.valueOf() < minDateTime.valueOf()
+  }, [minDateTime, start])
+  const beyondMax = useMemo(() => {
+    return maxDateTime && end.valueOf() > maxDateTime.valueOf()
+  }, [maxDateTime, end])
+  const beyondDuration = useMemo(() => {
+    if (maxDuration !== undefined) {
+      return end.valueOf() - start.valueOf() > maxDuration * 1000
+    }
+    return false
+  }, [maxDuration, start, end])
+
+  const updateDate = (dates: [Date, Date]) => {
+    const newStart = new Date(dates[0])
+    newStart.setHours(start.getHours())
+    newStart.setMinutes(start.getMinutes())
+    newStart.setSeconds(start.getSeconds())
+    setStart(newStart)
+
+    // to support to select the same day for start and end
+    let newEnd = new Date(dates[0])
+    if (dates[1]) {
+      newEnd = new Date(dates[1])
+    }
+    newEnd.setHours(end.getHours())
+    newEnd.setMinutes(end.getMinutes())
+    newEnd.setSeconds(end.getSeconds())
+    setEnd(newEnd)
+  }
+
+  const updateTime = (d: Date, setter: ReturnType<typeof useState<Date>>[1]) => {
+    setter((old) => {
+      const newD = new Date(old!)
+      newD.setHours(d.getHours())
+      newD.setMinutes(d.getMinutes())
+      newD.setSeconds(d.getSeconds())
+      return newD
+    })
+  }
+  const apply = () => onChange?.({ type: 'absolute', value: [dayjs(start).unix(), dayjs(end).unix()] })
+
+  return (
+    <div className="wd--m-1 wd-w-[280px] wd-p-4">
+      <Group className="wd-cursor-pointer" onClick={onReturnClick}>
+        <IconChevronLeft size={16} />
+        <Typography variant="body-lg">Back</Typography>
+      </Group>
+
+      <Group spacing={0} pt={8} position="apart">
+        <Typography variant="label-sm">Start</Typography>
+        <Group spacing={8}>
+          <DatePicker
+            onClick={() => {}}
+            w={116}
+            value={start}
+            inputFormat="MMM D, YYYY"
+            clearable={false}
+            error={beyondMin || startAfterEnd || beyondDuration}
+          />
+          <TimeInput
+            format="24"
+            w={90}
+            styles={{ input: { paddingLeft: 4, paddingRight: 4 } }}
+            withSeconds
+            value={start}
+            onChange={(d) => updateTime(d, setStart)}
+            error={beyondMin || startAfterEnd || beyondDuration}
+          />
+        </Group>
+      </Group>
+
+      <Group spacing={0} pt={8} position="apart">
+        <Typography variant="label-sm">End</Typography>
+        <Group spacing={8}>
+          <DatePicker
+            onClick={() => {}}
+            w={116}
+            value={end}
+            inputFormat="MMM D, YYYY"
+            clearable={false}
+            error={beyondMax || startAfterEnd || beyondDuration}
+          />
+          <TimeInput
+            format="24"
+            w={90}
+            styles={{ input: { paddingLeft: 4, paddingRight: 4 } }}
+            withSeconds
+            value={end}
+            onChange={(d) => updateTime(d, setEnd)}
+            error={beyondMax || startAfterEnd || beyondDuration}
+          />
+        </Group>
+      </Group>
+
+      <Flex justify="center" className="wd-pt-2" pt={8}>
+        <RangeCalendar
+          value={[start, end]}
+          onChange={updateDate}
+          maxDate={maxDateTime}
+          minDate={minDateTime}
+          dayStyle={dayStyle}
+        />
+      </Flex>
+
+      {(startAfterEnd || beyondMin || beyondMax || beyondDuration) && (
+        <Alert icon={<IconAlertCircle size={16} />} color="red" pt={8}>
+          {startAfterEnd && <Text color="red">Please select an end time after the start time.</Text>}
+          {beyondMin && (
+            <Text color="red">
+              Please select a start time after <>{timeFormatter(minDateTime!, null, 'MMM D, YYYY HH:mm:ss')}</>
+            </Text>
+          )}
+          {beyondMax && (
+            <Text color="red">
+              Please select an end time before <>{timeFormatter(maxDateTime!, null, 'MMM D, YYYY HH:mm:ss')}</>
+            </Text>
+          )}
+          {beyondDuration && (
+            <Text color="red">
+              The selection exceeds the {formatDuration(maxDuration!)} limit, please select a shorter time range.
+            </Text>
+          )}
+        </Alert>
+      )}
+
+      <Flex pt={8} gap="xs" justify="flex-end" align="flex-start" direction="row" wrap="wrap">
+        <Button size="xs" variant="default" onClick={onCancel}>
+          Cancel
+        </Button>
+        <Button size="xs" onClick={apply} disabled={startAfterEnd || beyondMin || beyondMax || beyondDuration}>
+          Apply
+        </Button>
+      </Flex>
+    </div>
+  )
+}
+
+export default AbsoluteTimeRangePicker
---
+++ packages/uikit/src/biz/TimeRangePicker/helpers.ts
@@ -0,0 +1,200 @@
+import dayjs from 'dayjs'
+import { trim } from 'lodash-es'
+
+export type TimeRange = RelativeTimeRange | AbsoluteTimeRange
+
+export interface RelativeTimeRange {
+  type: 'relative'
+  value: number // unit: seconds
+}
+
+export interface AbsoluteTimeRange {
+  type: 'absolute'
+  value: TimeRangeValue // unit: seconds
+}
+
+export type TimeRangeValue = [from: number, to: number]
+
+export const DEFAULT_QUICK_RANGES = [
+  3 * 24 * 60 * 60,
+  2 * 24 * 60 * 60,
+  24 * 60 * 60,
+  12 * 60 * 60,
+  3 * 60 * 60,
+  60 * 60,
+  30 * 60,
+  15 * 60,
+  5 * 60
+]
+
+export const DEFAULT_TIME_RANGE: TimeRange = {
+  type: 'relative',
+  value: 30 * 60
+}
+
+const FORMAT_TOKENS: { [k: string]: { unit: Intl.RelativeTimeFormatUnit; fnName: string } } = {
+  s: {
+    unit: 'second',
+    fnName: 'asSeconds'
+  },
+  m: {
+    unit: 'minute',
+    fnName: 'asMinutes'
+  },
+  h: {
+    unit: 'hour',
+    fnName: 'asHours'
+  },
+  d: {
+    unit: 'day',
+    fnName: 'asDays'
+  },
+  mo: {
+    unit: 'month',
+    fnName: 'asMonths'
+  }
+}
+
+export const formatDuration = (seconds: number, short = false) => {
+  let token = 's'
+  if (seconds >= 30 * 24 * 60 * 60) {
+    token = 'mo'
+  } else if (seconds >= 24 * 60 * 60) {
+    token = 'd'
+  } else if (seconds >= 60 * 60) {
+    token = 'h'
+  } else if (seconds >= 60) {
+    token = 'm'
+  }
+
+  const durationLength = Math.ceil(dayjs.duration(seconds, 'seconds')[FORMAT_TOKENS[token].fnName]())
+
+  if (short) {
+    return `${durationLength}${token}`
+  }
+
+  return `${durationLength}${formatUnit(durationLength, FORMAT_TOKENS[token].unit)}`
+}
+
+const formatUnit = (seconds: number, unit: Intl.RelativeTimeFormatUnit) => {
+  const rtf = new Intl.RelativeTimeFormat('en', { numeric: 'always' })
+  const parts = rtf.formatToParts(seconds, unit)
+  return parts[parts.length - 1].value
+}
+
+export const toTimeRangeValue = (timeRange: TimeRange, offset = 0): TimeRangeValue => {
+  if (timeRange.type === 'absolute') {
+    return timeRange.value.map((t) => t + offset) as TimeRangeValue
+  } else {
+    const now = dayjs().unix()
+    return [now - timeRange.value + offset, now + offset]
+  }
+}
+
+export function fromTimeRangeValue(v: TimeRangeValue): AbsoluteTimeRange {
+  return {
+    type: 'absolute',
+    value: [...v]
+  }
+}
+
+export type URLTimeRange = { from: string; to: string }
+
+export const toURLTimeRange = (timeRange: TimeRange): URLTimeRange => {
+  if (timeRange.type === 'relative') {
+    return { from: `${timeRange.value}`, to: 'now' }
+  }
+
+  const timeRangeValue = toTimeRangeValue(timeRange)
+  return { from: `${timeRangeValue[0]}`, to: `${timeRangeValue[1]}` }
+}
+
+export const urlToTimeRange = (urlTimeRange: URLTimeRange): TimeRange => {
+  if (urlTimeRange.to === 'now') {
+    return { type: 'relative', value: Number(urlTimeRange.from) }
+  }
+  return { type: 'absolute', value: [Number(urlTimeRange.from), Number(urlTimeRange.to)] }
+}
+
+export const urlToTimeRangeValue = (urlTimeRange: URLTimeRange): TimeRangeValue => {
+  return toTimeRangeValue(urlToTimeRange(urlTimeRange))
+}
+
+//----------
+
+export const DEFAULT_TIME_FORMAT = 'YYYY-MM-DD HH:mm:ss'
+export const DEFAULT_TIME_FORMAT_WITH_TIMEZONE = 'YYYY-MM-DD HH:mm:ss Z'
+
+// add utcOffset for date time
+export function addOffsetUTC(time: string | number | Date, utcOffset: number) {
+  const targetTime = new Date(time)
+
+  // Get the timezone offset from local time in minutes
+  const tzDifference = utcOffset * 60 + targetTime.getTimezoneOffset()
+  // Convert the offset to milliseconds, add to targetTime, and make a new Date
+  return new Date(targetTime.getTime() + tzDifference * 60 * 1000)
+}
+
+// display UTC offset
+// number: -9, 7, 0, 8.5
+export const getUTCString = (offset: number) => {
+  const offsetStrs = offset.toString().split('.')
+
+  let mm = '00'
+  let hh = ''
+
+  const offsetH = Number.parseInt(offsetStrs[0] || '0')
+  const offsetM = Number.parseFloat(`0.${offsetStrs[1]}` || '0') * 60
+
+  hh = Math.abs(offsetH).toString().padStart(2, '0')
+  mm = offsetM === 0 ? '00' : offsetM.toString()
+
+  if (offset > 0) {
+    return `UTC+${hh}:${mm}`
+  }
+  if (offset < 0) {
+    return `UTC-${hh}:${mm}`
+  }
+
+  return 'UTC±00:00'
+}
+
+export const formatTime = (
+  timeValue: number | string | Date,
+  options: {
+    offset?: number
+    showSeconds?: boolean
+    showTimezone?: boolean
+  } = {}
+) => {
+  const { showSeconds = true, showTimezone = false, offset } = options
+  let format = showSeconds ? DEFAULT_TIME_FORMAT : `YYYY-MM-DD HH:mm`
+  if (showTimezone) {
+    format += ' Z'
+  }
+  return timeFormatter(timeValue, offset, format)
+}
+
+export const timeFormatter = (
+  timeValue: number | string | Date,
+  utcOffset?: number | null,
+  format = DEFAULT_TIME_FORMAT
+) => {
+  // null, undefined, 0, ''
+  if (!timeValue) return '-'
+  // convert unix timestamp number/string(seconds) to milliseconds
+  // time schema in billing services is unix timestamp string, such as: { time: "1602201600" }
+  const time = timeValue instanceof Date || isNaN(Number(timeValue)) ? timeValue : Number(timeValue) * 1000
+  // use a localized timezone of the user agent(browser) if utcOffset is unavailable
+  // https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Date/getTimezoneOffset
+  const currentTZOffsetInHours =
+    utcOffset === undefined || utcOffset === null ? -(new Date().getTimezoneOffset() / 60) : utcOffset
+  const withUTC = format.indexOf('Z') >= 0 // display UTC offset
+
+  // use a specific timezone with utcOffset
+  // use customized UTC offset display format, like Dec 14, 2021 08:03 UTC+08:00
+  return (
+    dayjs(addOffsetUTC(time, currentTZOffsetInHours)).format(withUTC ? trim(format, 'Z') : format) +
+    (withUTC ? getUTCString(currentTZOffsetInHours) : '')
+  )
+}
