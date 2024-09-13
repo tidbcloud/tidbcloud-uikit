dafdf24ad1e55ec13ad91f539c623bd6eef63cdf
awxxxxxx <dwaterbear@163.com>
2024-09-13 11:18:23 +0800

    feat: sync phone input styles

--- packages/uikit/src/biz/PhoneInput/styles.ts
+++ packages/uikit/src/biz/PhoneInput/styles.ts
@@ -1,4 +1,4 @@
-import { createStyles } from '@mantine/core'
+import { CSSObject, createStyles } from '@mantine/core'
 
 export const useStyles = createStyles((theme, params: { hasError?: boolean }) => {
   const { hasError = true } = params
@@ -7,6 +7,21 @@ export const useStyles = createStyles((theme, params: { hasError?: boolean }) =>
     variant: 'filled',
     color: 'red'
   }).background
+
+  const invalidStyles: CSSObject = {
+    borderColor: theme.colors.red[4],
+
+    '&:hover': {
+      borderColor: theme.colors.red[4]
+    },
+    '&:focus': {
+      borderColor: theme.colors.red[4]
+    },
+    '&::placeholder': {
+      ...theme.fn.placeholderStyles(),
+      color: invalidColor
+    }
+  }
 
   return {
     dropdownButton: {
@@ -18,39 +33,36 @@ export const useStyles = createStyles((theme, params: { hasError?: boolean }) =>
         '.form-control': {
           ...theme.fn.fontStyles(),
           // see more https://github.com/mantinedev/mantine/blob/v6/src/mantine-core/src/Input/Input.styles.ts#L30-L187
-          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.white,
+          backgroundColor: theme.colorScheme === 'dark' ? theme.colors.carbon[0] : theme.white,
           transition: 'border-color 100ms ease',
-          '&:focus, &:focus-within': {
-            ...theme.focusRingStyles.inputStyles(theme)
-          },
 
           fontSize: 14,
-          color: hasError ? invalidColor : theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+          color: theme.colors.carbon[8],
           width: '100%',
           height: 40,
           minHeight: 40,
           lineHeight: '38px',
           borderRadius: theme.defaultRadius,
           padding: '0 12px',
-          border: hasError
-            ? `1px solid ${invalidColor}`
-            : `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4]}`,
+          borderWidth: 1,
+          borderStyle: 'solid',
+          borderColor: theme.colors.carbon[4],
           marginBottom: hasError ? 5 : undefined,
           outline: 'none',
 
-          '::placeholder': {
-            ...theme.fn.placeholderStyles(),
-            opacity: 1,
-            ...(hasError
-              ? {
-                  opacity: 1,
-                  color: invalidColor
-                }
-              : undefined)
-          },
           '&:hover': {
-            borderColor: hasError ? theme.colors.red[6] : undefined
-          }
+            borderColor: theme.colors.carbon[5]
+          },
+
+          '&:focus, &:focus-within': {
+            borderColor: theme.colors.carbon[9]
+          },
+
+          '&::placeholder': {
+            ...theme.fn.placeholderStyles(),
+            opacity: 1
+          },
+          ...(hasError ? invalidStyles : undefined)
         }
       }
     }
