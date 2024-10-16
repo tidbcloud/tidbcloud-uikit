cd52239e972b97019cc364745c6cc4fb4753061f
Yuiham <Yuiham@users.noreply.github.com>
2024-10-16 12:05:15 +0800

    refactor(uikit/utils): adapt to mantine v7

--- packages/uikit/src/utils/index.ts
+++ packages/uikit/src/utils/index.ts
@@ -1,4 +1,2 @@
-export * from '@mantine/utils'
-
 export * from './styles.js'
 export * from './dayjs.js'
--- packages/uikit/src/utils/styles.ts
+++ packages/uikit/src/utils/styles.ts
@@ -1,33 +1,35 @@
-import { CSSObject, DefaultProps, MantineTheme } from '@mantine/core'
+import { MantineTheme } from '@mantine/core'
+import { CSSObject, EmotionHelpers, EmotionStyles, EmotionSx } from '@mantine/emotion'
 import { merge } from 'lodash-es'
 
-function mergeSxValues(values: DefaultProps['sx'][], theme: MantineTheme) {
+function mergeSxValues(values: EmotionSx[], theme: MantineTheme, u: EmotionHelpers) {
   const css: CSSObject = values.reduce<CSSObject>((ret, partial) => {
     if (!partial) {
       return ret
     }
 
     if (Array.isArray(partial)) {
-      return mergeSxValues(partial, theme)
+      return mergeSxValues(partial, theme, u)
     }
 
-    return merge(ret, typeof partial === 'function' ? partial(theme) : partial)
+    return merge(ret, typeof partial === 'function' ? partial(theme, u) : partial)
   }, {})
 
   return css
 }
 
-export function mergeSxList(sxList: DefaultProps['sx'][]) {
-  return (theme: MantineTheme) => mergeSxValues(sxList, theme)
+export function mergeSxList(sxList: EmotionSx[]) {
+  return (theme: MantineTheme, u: EmotionHelpers) => mergeSxValues(sxList, theme, u)
 }
 
 export function mergeStylesList<
   StylesNames extends string = never,
   StylesParams extends Record<string, any> = Record<string, any>
->(stylesList: DefaultProps<StylesNames, StylesParams>['styles'][]) {
-  return (theme: MantineTheme, params: StylesParams) => {
+>(stylesList: EmotionStyles[]) {
+  return (theme: MantineTheme, u: EmotionHelpers, params: StylesParams) => {
     const css = stylesList.reduce<Partial<Record<StylesNames, CSSObject>>>(
-      (prev, partial) => (typeof partial === 'function' ? merge(prev, partial(theme, params)) : merge(prev, partial)),
+      (prev, partial) =>
+        typeof partial === 'function' ? merge(prev, partial(theme, params, u)) : merge(prev, partial),
       {}
     )
 
