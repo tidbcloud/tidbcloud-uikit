b3d42e36a88e87282345cbc3a6c78b9880a9b9f9
Yuiham <Yuiham@users.noreply.github.com>
2024-10-16 17:10:19 +0800

    refactor(uikit/primitive/notifier): adapt to mantine v7

--- packages/uikit/src/primitive/notifier/index.ts
+++ packages/uikit/src/primitive/notifier/index.ts
@@ -1,15 +1,15 @@
-import { showNotification, cleanNotifications, NotificationProps } from '@mantine/notifications'
+import { showNotification, cleanNotifications, NotificationData } from '@mantine/notifications'
 
 export * from '@mantine/notifications'
 
 export const notifier = {
-  success: (message: NotificationProps['message'], props?: Omit<NotificationProps, 'message'>) =>
+  success: (message: NotificationData['message'], props?: Omit<NotificationData, 'message'>) =>
     showNotification({ message, color: 'green', ...props }),
-  warn: (message: NotificationProps['message'], props?: Omit<NotificationProps, 'message'>) =>
+  warn: (message: NotificationData['message'], props?: Omit<NotificationData, 'message'>) =>
     showNotification({ message, color: 'yellow', ...props }),
-  error: (message: NotificationProps['message'], props?: Omit<NotificationProps, 'message'>) =>
+  error: (message: NotificationData['message'], props?: Omit<NotificationData, 'message'>) =>
     showNotification({ message, color: 'red', ...props }),
-  info: (message: NotificationProps['message'], props?: Omit<NotificationProps, 'message'>) =>
+  info: (message: NotificationData['message'], props?: Omit<NotificationData, 'message'>) =>
     showNotification({ message, color: 'cyan', ...props }),
   clear: () => cleanNotifications()
 }
