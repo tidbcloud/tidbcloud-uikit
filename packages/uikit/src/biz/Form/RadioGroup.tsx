c01d8957769fa82d8c48acfc0eed2727ca110ce9
zoubingwu <zoubingwu@gmail.com>
2024-12-07 00:42:31 +0800

    feat(RadioGroup): enhance FormRadioGroup with tooltip support and flexible layout options
    - Refactored FormRadioGroup to accept orientation and gap props for layout customization.
    - Introduced tooltip functionality for radio options, allowing the use of Tooltip or HoverCard components.
    - Updated storybook to demonstrate the new FormRadioGroup with various radio options and tooltips.

--- packages/uikit/src/biz/Form/RadioGroup.tsx
+++ packages/uikit/src/biz/Form/RadioGroup.tsx
@@ -1,17 +1,48 @@
 import { ErrorMessage } from '@hookform/error-message'
-import { get, omit } from 'lodash-es'
+import { omit } from 'lodash-es'
 import React, { Fragment } from 'react'
 import { Controller, RegisterOptions, useFormContext } from 'react-hook-form'
 
-import { Radio, RadioGroupProps, RadioProps, Box, HoverCard, HoverCardProps, Tooltip } from '../../primitive/index.js'
+import {
+  Radio,
+  RadioGroupProps,
+  RadioProps,
+  HoverCard,
+  HoverCardProps,
+  Tooltip,
+  Flex,
+  FlexProps,
+  Box
+} from '../../primitive/index.js'
+
+interface RadioGroupItemData extends RadioProps {
+  tooltip?: React.ReactNode
+  tooltipProps?: {
+    /**
+     * By default it will use HoverCard component to render tooltips.
+     * Set this to `true` to use Tooltip component instead.
+     */
+    useTooltip?: boolean
+  } & HoverCardProps
+}
 
 export interface FormRadioGroupProps extends Omit<RadioGroupProps, 'children'> {
   name: string
   rules?: RegisterOptions
-  data: Array<RadioProps & { tooltip?: React.ReactNode; tooltipProps?: HoverCardProps & { useTooltip?: boolean } }>
+  data: RadioGroupItemData[]
+  direction?: FlexProps['direction']
+  gap?: FlexProps['gap']
 }
 
-export const FormRadioGroup: React.FC<FormRadioGroupProps> = ({ name, rules, onChange, data, ...rest }) => {
+export const FormRadioGroup = ({
+  name,
+  rules,
+  onChange,
+  data,
+  direction,
+  gap = 'md',
+  ...rest
+}: FormRadioGroupProps) => {
   const { control, formState, getFieldState } = useFormContext()
   const { error } = getFieldState(name, formState)
 
@@ -32,34 +63,34 @@ export const FormRadioGroup: React.FC<FormRadioGroupProps> = ({ name, rules, onC
             {...restField}
             error={error ? <ErrorMessage errors={formState.errors} name={name} /> : undefined}
           >
-            {data.map((i) => {
-              const radioProps = omit(i, 'tooltipProps', 'tooltip')
-              return (
-                <Fragment key={i.value as any}>
-                  {i.tooltip ? (
-                    i.tooltipProps?.useTooltip ? (
-                      <Tooltip label={i.tooltip} withArrow>
-                        <Box>
+            <Flex direction={direction} gap={gap} mt="xs">
+              {data.map((i) => {
+                const radioProps = omit(i, 'tooltipProps', 'tooltip')
+                return (
+                  <Fragment key={i.value as any}>
+                    {i.tooltip ? (
+                      i.tooltipProps?.useTooltip ? (
+                        <Tooltip label={i.tooltip} withArrow refProp="rootRef">
                           <Radio {...radioProps} />
-                        </Box>
-                      </Tooltip>
-                    ) : (
-                      <HoverCard withArrow shadow="md" withinPortal {...i.tooltipProps}>
-                        <HoverCard.Target>
-                          <Box>
-                            <Radio {...radioProps} />
-                          </Box>
-                        </HoverCard.Target>
+                        </Tooltip>
+                      ) : (
+                        <HoverCard withArrow shadow="md" withinPortal {...i.tooltipProps}>
+                          <HoverCard.Target>
+                            <Box>
+                              <Radio {...radioProps} />
+                            </Box>
+                          </HoverCard.Target>
 
-                        <HoverCard.Dropdown>{i.tooltip}</HoverCard.Dropdown>
-                      </HoverCard>
-                    )
-                  ) : (
-                    <Radio {...radioProps} />
-                  )}
-                </Fragment>
-              )
-            })}
+                          <HoverCard.Dropdown>{i.tooltip}</HoverCard.Dropdown>
+                        </HoverCard>
+                      )
+                    ) : (
+                      <Radio {...radioProps} />
+                    )}
+                  </Fragment>
+                )
+              })}
+            </Flex>
           </Radio.Group>
         )
       }}
