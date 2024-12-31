335c4aa126d7995d94f5317e0c5f1f3e1111e4c0
zoubingwu <zoubingwu@gmail.com>
2024-12-31 13:40:02 +0800

    refactor(documentation): update emotion imports and remove unused dependency
    - Removed '@emotion/server' from package.json and pnpm-lock.yaml as it is no longer needed.
    - Updated import paths in `emotion.ts` and `_document.tsx` to reflect the new module structure for emotion utilities.
    - These changes streamline the documentation and ensure correct usage of emotion in the project.

--- packages/documentation/lib/emotion.ts
+++ packages/documentation/lib/emotion.ts
@@ -1,3 +1,3 @@
-import { createEmotionCache } from '@tidbcloud/uikit/utils'
+import { createEmotionCache } from '@tidbcloud/uikit/emotion'
 
 export const emotionCache = createEmotionCache({ key: 'css' })
