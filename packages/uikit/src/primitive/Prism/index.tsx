4a77f753edf167694c68ebe30c01ab923607eb0d
Suhaha <jklopsdfw@gmail.com>
2025-01-13 14:07:13 +0800

    chore: export language type of prism

--- packages/uikit/src/primitive/Prism/index.tsx
+++ packages/uikit/src/primitive/Prism/index.tsx
@@ -1,4 +1,4 @@
 export { Prism } from './Prism.js'
 export type { PrismProps, PrismStylesNames } from './Prism.js'
 export type { PrismStylesParams } from './Prism.styles.js'
-export type { PrismTheme } from 'prism-react-renderer'
+export type { PrismTheme, Language } from 'prism-react-renderer'
