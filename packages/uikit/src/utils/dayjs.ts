8d739bee666c349b4b1f7a429803ca8ed1817310
zoubingwu <zoubingwu@gmail.com>
2025-02-20 12:13:16 +0800

    refactor(uikit): update dayjs import and export strategy

--- packages/uikit/src/utils/dayjs.ts
+++ packages/uikit/src/utils/dayjs.ts
@@ -1,16 +1,15 @@
-import dayjs from 'dayjs'
+import _dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat'
 import duration from 'dayjs/plugin/duration'
 import minMax from 'dayjs/plugin/minMax'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
 
-dayjs.extend(duration)
-dayjs.extend(minMax)
-dayjs.extend(customParseFormat)
-dayjs.extend(utc)
-dayjs.extend(timezone)
+_dayjs.extend(duration)
+_dayjs.extend(minMax)
+_dayjs.extend(customParseFormat)
+_dayjs.extend(utc)
+_dayjs.extend(timezone)
 
-export { dayjs }
-
+export const dayjs = _dayjs
 export type { Dayjs } from 'dayjs'
