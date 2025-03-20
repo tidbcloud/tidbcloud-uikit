1ae53148a9d9023bb250babdc7909ef9ca510253
zoubingwu <zoubingwu@gmail.com>
2025-03-20 21:20:16 +0800

    update icon jsx

--- packages/uikit/src/icons/react/Database.jsx
+++ packages/uikit/src/icons/react/Database.jsx
@@ -13,7 +13,7 @@ const IconDatabase = (props, ref) => {
       {...props}
     >
       <path
-        stroke="#F2AADA"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M14 3.334c0 1.104-2.686 2-6 2s-6-.896-6-2m12 0c0-1.105-2.686-2-6-2s-6 .895-6 2m12 0v9.333c0 1.107-2.667 2-6 2s-6-.893-6-2V3.334M14 8c0 1.107-2.667 2-6 2s-6-.893-6-2"
