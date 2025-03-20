5aa23862057028ff285bc25d7317767560933800
zoubingwu <zoubingwu@gmail.com>
2025-03-20 21:20:16 +0800

    update icon jsx

--- packages/uikit/src/icons/react/Brackets.jsx
+++ packages/uikit/src/icons/react/Brackets.jsx
@@ -13,7 +13,7 @@ const IconBrackets = (props, ref) => {
       {...props}
     >
       <path
-        stroke="#999"
+        stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M12.38 13.333c.842 0 1.525-.682 1.525-1.524V8.762L14.667 8l-.762-.762V4.191c0-.842-.682-1.524-1.524-1.524m-8.761 0c-.843 0-1.525.682-1.525 1.524v3.047L1.334 8l.763.762v3.047c0 .842.681 1.524 1.523 1.524"
