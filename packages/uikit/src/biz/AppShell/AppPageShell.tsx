6873737073a23ffcf8e1212883a2a350ea723247
Yuiham <Yuiham@users.noreply.github.com>
2025-04-10 13:32:47 +0800

    feat(uikit/AppShell): add `AppPageShell` for flexible layout with optional header

---
+++ packages/uikit/src/biz/AppShell/AppPageShell.tsx
@@ -0,0 +1,75 @@
+import { mergeSxList } from '../../utils/index.js'
+import { PageShell, type PageShellProps } from '../PageShell/index.js'
+
+export interface AppPageShellProps
+  extends Pick<PageShellProps, 'title' | 'wrapperProps' | 'headerProps' | 'bodyProps' | 'children'> {
+  withHeader?: boolean
+}
+
+export const AppPageShell = ({
+  withHeader = true,
+  headerProps,
+  bodyProps,
+  wrapperProps,
+  ...rest
+}: AppPageShellProps) => {
+  if (!withHeader) {
+    return (
+      <PageShell
+        bodyProps={{
+          ...bodyProps,
+          sx: mergeSxList([
+            {
+              height: '100%',
+              maxWidth: `calc(1920px - var(--app-shell-navbar-width))`,
+              margin: '0 auto'
+            },
+            bodyProps?.sx
+          ])
+        }}
+        {...rest}
+      />
+    )
+  }
+
+  return (
+    <PageShell
+      wrapped
+      wrapperProps={{
+        ...wrapperProps,
+        sx: mergeSxList([
+          (theme) => ({
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            maxWidth: `calc(1920px - var(--app-shell-navbar-width))`,
+            margin: '0 auto',
+            minWidth: `calc(${theme.breakpoints.md} - var(--app-shell-navbar-width))`
+          }),
+          wrapperProps?.sx
+        ])
+      }}
+      headerProps={{
+        ...headerProps,
+        sx: mergeSxList([
+          {
+            flexShrink: 0
+          },
+          headerProps?.sx
+        ])
+      }}
+      bodyProps={{
+        ...bodyProps,
+        sx: mergeSxList([
+          {
+            paddingTop: 16,
+            paddingBottom: 64,
+            flex: 1
+          },
+          bodyProps?.sx
+        ])
+      }}
+      {...rest}
+    />
+  )
+}
