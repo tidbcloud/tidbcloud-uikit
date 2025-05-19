daa3660c45904330fdf8118a5d783d4ed62730c6
Yuiham <Yuiham@users.noreply.github.com>
2025-05-19 15:10:38 +0800

    refactor(uikit/AppShell): enhance AppShell components with className support for improved styling

--- packages/uikit/src/biz/AppShell/container/AppShellRoot.tsx
+++ packages/uikit/src/biz/AppShell/container/AppShellRoot.tsx
@@ -2,7 +2,7 @@ import { Stack } from '../../../primitive/index.js'
 
 export const AppShellRoot = ({ children }: React.PropsWithChildren) => {
   return (
-    <Stack gap={0} sx={{ height: '100vh', overflow: 'hidden' }}>
+    <Stack className="tiui-app-shell-root" gap={0} sx={{ height: '100vh', overflow: 'hidden' }}>
       {children}
     </Stack>
   )
