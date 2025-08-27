fa732add7ee391183e1fa2aafefdbba9fde1e343
zoubingwu <zoubingwu@gmail.com>
2025-08-27 13:41:42 +0800

    fix: enhance CollapsibleCard with customizable header and content props (#586)

--- packages/uikit/src/biz/CollapsibleCard/index.tsx
+++ packages/uikit/src/biz/CollapsibleCard/index.tsx
@@ -1,21 +1,39 @@
 import { useDisclosure } from '../../hooks/index.js'
 import { IconChevronRight } from '../../icons/index.js'
-import { Box, Card, CardProps, Collapse, Group } from '../../primitive/index.js'
+import { Box, BoxProps, Card, CardProps, Collapse, Group, GroupProps } from '../../primitive/index.js'
 
 export interface CollapsibleCardProps extends CardProps {
   title: React.ReactNode
   children: React.ReactNode
-
   defaultOpened?: boolean
+  headerProps?: GroupProps
+  contentProps?: BoxProps
 }
 
-export const CollapsibleCard = ({ children, title, defaultOpened = false }: CollapsibleCardProps) => {
+export const CollapsibleCard = ({
+  children,
+  title,
+  defaultOpened = false,
+  headerProps,
+  contentProps
+}: CollapsibleCardProps) => {
   const [opened, { toggle }] = useDisclosure(defaultOpened)
 
   return (
     <Card>
       <Card.Section>
-        <Group onClick={toggle} align="center" sx={{ cursor: 'pointer', userSelect: 'none' }} p={16} bg="carbon.2">
+        <Group
+          onClick={toggle}
+          align="center"
+          sx={{
+            cursor: 'pointer',
+            userSelect: 'none',
+            borderBottom: opened ? '1px solid var(--mantine-color-carbon-3)' : undefined
+          }}
+          p={16}
+          bg="carbon.2"
+          {...headerProps}
+        >
           <IconChevronRight
             size={18}
             sx={{
@@ -30,7 +48,9 @@ export const CollapsibleCard = ({ children, title, defaultOpened = false }: Coll
 
       <Card.Section>
         <Collapse in={opened}>
-          <Box p={16}>{children}</Box>
+          <Box p={16} {...contentProps}>
+            {children}
+          </Box>
         </Collapse>
       </Card.Section>
     </Card>
