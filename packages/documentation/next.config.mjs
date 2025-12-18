6bf9bd69cc7ce66085093dc3f70b2d24a90e4685
zoubingwu <zoubingwu@gmail.com>
2025-12-19 00:27:17 +0800

    refactor(docs): remove search configuration from Nextra setup

--- packages/documentation/next.config.mjs
+++ packages/documentation/next.config.mjs
@@ -3,9 +3,6 @@ import nextra from 'nextra'
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  search: {
-    codeblocks: false
-  },
   defaultShowCopyCode: true
 })
 
